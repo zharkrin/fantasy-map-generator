fb3f4ad43a19ae29c61e9b1344f7802c0fa10ef2
zharkrin <zarkrin2@gmail.com>
2026-06-14 17:42:44 +0200

    Crear config.js

---
+++ frontend/js/core/config.js
@@ -0,0 +1,93 @@
+export const CONFIG_ESCALAS = {
+
+    1:{
+        ancho:4096,
+        alto:2048,
+        continentes:"1 - 3",
+        islas:"10 - 30",
+        rios:"20 - 80",
+        naciones:"5 - 20"
+    },
+
+    2:{
+        ancho:8192,
+        alto:4096,
+        continentes:"2 - 5",
+        islas:"20 - 60",
+        rios:"50 - 150",
+        naciones:"10 - 40"
+    },
+
+    3:{
+        ancho:12288,
+        alto:6144,
+        continentes:"3 - 7",
+        islas:"30 - 90",
+        rios:"100 - 250",
+        naciones:"20 - 60"
+    },
+
+    4:{
+        ancho:16384,
+        alto:8192,
+        continentes:"4 - 9",
+        islas:"40 - 120",
+        rios:"150 - 350",
+        naciones:"30 - 80"
+    },
+
+    5:{
+        ancho:20480,
+        alto:10240,
+        continentes:"5 - 12",
+        islas:"50 - 150",
+        rios:"250 - 500",
+        naciones:"40 - 120"
+    },
+
+    6:{
+        ancho:24576,
+        alto:12288,
+        continentes:"6 - 14",
+        islas:"70 - 180",
+        rios:"350 - 700",
+        naciones:"60 - 160"
+    },
+
+    7:{
+        ancho:28672,
+        alto:14336,
+        continentes:"7 - 16",
+        islas:"90 - 220",
+        rios:"500 - 900",
+        naciones:"80 - 220"
+    },
+
+    8:{
+        ancho:32768,
+        alto:16384,
+        continentes:"8 - 18",
+        islas:"110 - 260",
+        rios:"700 - 1100",
+        naciones:"120 - 300"
+    },
+
+    9:{
+        ancho:36864,
+        alto:18432,
+        continentes:"9 - 20",
+        islas:"130 - 300",
+        rios:"900 - 1400",
+        naciones:"160 - 400"
+    },
+
+    10:{
+        ancho:40960,
+        alto:20480,
+        continentes:"10 - 25",
+        islas:"150 - 400",
+        rios:"1200 - 1800",
+        naciones:"200 - 500"
+    }
+
+};
