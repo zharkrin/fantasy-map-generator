6ed40478f195643167150cadd84513310d97d737
zharkrin <zarkrin2@gmail.com>
2026-06-14 18:39:07 +0200

    Crear estadoGlobal.js

---
+++ frontend/js/core/estadoGlobal.js
@@ -0,0 +1,23 @@
+export const estadoGlobal = {
+
+    mundo:{
+
+        nombre:"Nuevo Mundo",
+
+        semilla:0,
+
+        escalaMundo:1,
+
+        ancho:4096,
+
+        alto:2048,
+
+        altura:[],
+        rios:[],
+        ciudades:[],
+        naciones:[],
+        culturas:[],
+        caminos:[]
+    }
+
+};
