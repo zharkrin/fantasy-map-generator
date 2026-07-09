767c3e154eaeecb840838696b0f7cf83d59bc779
zharkrin <zarkrin2@gmail.com>
2026-07-09 17:49:37 +0200

    Crear index.js

---
+++ frontend/js/mapa/terreno/costas/index.js
@@ -0,0 +1,22 @@
+/*
+==========================================================
+Proyecto : Fantasy Map Generator
+Archivo  : index.js
+Ruta     : frontend/js/mapa/terreno/costas/
+Autor    : OpenAI + Asmodeus
+Licencia : MIT
+==========================================================
+
+Punto de entrada del módulo de costas.
+
+Centraliza todas las exportaciones relacionadas
+con la detección y procesamiento de las costas.
+
+==========================================================
+*/
+
+export { detectarCostas } from "./detectarCostas.js";
+
+export { detectarPlayas } from "./playas.js";
+
+export { suavizarCostas } from "./suavizarCostas.js";
